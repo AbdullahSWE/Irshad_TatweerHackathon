a8efe8ed8a3af1d8bc40591295fd7762d17d1f88
AbdullahSWE <abdullahaftab01@gmail.com>
2026-06-26 15:57:53 +0400

    Initial Commit

---
+++ Irshad/IrshadApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrshadApp.swift
+//  Irshad
+//
+//  Created by pac on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct IrshadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
